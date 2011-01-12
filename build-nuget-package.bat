e27a8a666e75e2fb9000f2ee73f6d4de4a979e16
Andrew Davey <andrew@equin.co.uk>
2011-01-12 12:00:31 +0000

    Create nuget package script

---
+++ build-nuget-package.bat
@@ -0,0 +1,19 @@
+echo Build package v%1
+
+pushd .
+cd tools\nuget
+
+rmdir /S /Q package
+
+mkdir package
+mkdir package\lib
+mkdir package\lib\net40
+
+copy postal.nuspec package\postal.nuspec
+copy ..\..\src\postal\bin\release\postal.dll package\lib\net40
+
+nuget pack package\postal.nuspec
+
+rmdir /S /Q package
+
+popd
