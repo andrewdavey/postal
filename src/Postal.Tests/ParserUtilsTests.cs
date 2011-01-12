d72bc51a4ccbb5ec01357a49545212789c6a4387
Andrew Davey <andrew@equin.co.uk>
2011-01-12 15:05:20 +0000

    Add tests project. Waiting for openwrap xunit and tdnet support.

---
+++ src/Postal.Tests/ParserUtilsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using System.IO;
+
+namespace Postal
+{
+    public class ParserUtilsTests
+    {
+        [Fact]
+        public void Can_parse_header()
+        {
+            var input = "hello: world";
+
+            Dictionary<string, string> headers = new Dictionary<string, string>();
+            using (var reader = new StringReader(input))
+            {
+                ParserUtils.ParseHeaders(reader, (key, value) => headers.Add(key, value));
+            }
+
+            Assert.Equal(headers["hello"], "world");
+        }
+
+
+    }
+}
--- src/Postal.Tests/Properties/AssemblyInfo.cs
+++ src/Postal.Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Postal")]
+[assembly: AssemblyTitle("Postal.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Postal")]
-[assembly: AssemblyCopyright("Copyright © Andrew Davey 2010")]
+[assembly: AssemblyProduct("Postal.Tests")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5279c130-0531-4cb5-9fea-39406a93443b")]
+[assembly: Guid("3ebf13cc-2e61-4bff-85da-37851417d413")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,8 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-
-
-[assembly: InternalsVisibleTo("Postal.Tests")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
