b7defa283e561eeafbf0fca85ff9bfe36bbc1af3
Andrew Davey <andrew@equin.co.uk>
2014-06-25 19:37:30 +0100

    Use Stream.CopyTo

--- src/Postal/ImageEmbedder.cs
+++ src/Postal/ImageEmbedder.cs
@@ -141,14 +141,9 @@ namespace Postal
 
         static byte[] ReadFully(Stream input)
         {
-            byte[] buffer = new byte[16 * 1024];
-            using (MemoryStream ms = new MemoryStream())
+            using (var ms = new MemoryStream())
             {
-                int read;
-                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
-                {
-                    ms.Write(buffer, 0, read);
-                }
+                input.CopyTo(ms);
                 return ms.ToArray();
             }
         }
