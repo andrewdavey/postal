7111f749b53a2a4c2cd819e4950a484878c653f6
herman <hmcherman@gmail.com>
2018-02-09 12:05:13 +0800

    Nuget Dependencies update

---
+++ dotnet_build.cmd
@@ -0,0 +1,3 @@
+dotnet build -c Release ./src/Postal.AspNetCore/Postal.AspNetCore.csproj
+dotnet pack -c Release ./RazorEngine/src/source/RazorEngine.Core/RazorEngine.Core.NetCore.csproj
+dotnet pack -c Release ./src/Postal.AspNetCore/Postal.AspNetCore.csproj
