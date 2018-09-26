e8594e664eb1c108de587a84e42bf4e508275242
Herman Ho <hmcherman@gmail.com>
2018-09-26 15:44:07 +0800

    Undo PathBase :(

--- src/Postal.AspNetCore/TemplateServices/TemplateService.cs
+++ src/Postal.AspNetCore/TemplateServices/TemplateService.cs
@@ -38,15 +38,7 @@ namespace Postal.AspNetCore
             {
                 httpContext.Request.Host = viewModel.RequestPath.Host;
                 httpContext.Request.Scheme = viewModel.RequestPath.Scheme;
-                if (viewModel.RequestPath.Scheme != null && viewModel.RequestPath.Host != null)
-                {
-                    //include fqdn
-                    httpContext.Request.PathBase = $"{viewModel.RequestPath.Scheme}://{viewModel.RequestPath.Host}{viewModel.RequestPath.PathBase}";
-                }
-                else
-                {
-                    httpContext.Request.PathBase = viewModel.RequestPath.PathBase;
-                }
+                httpContext.Request.PathBase = viewModel.RequestPath.PathBase;
             }
 
             var actionDescriptor = new ActionDescriptor
