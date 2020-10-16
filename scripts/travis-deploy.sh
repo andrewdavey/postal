64d28b7d83cd197770333452e5febb154460cb76
Herman Ho <hermanho@users.noreply.github.com>
2020-10-17 01:01:06 +0800

    fix travis-ci

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -1,3 +1,3 @@
-dotnet nuget add source "https://nuget.pkg.github.com/hermanho/index.json" -Name "GitHub" -UserName hermanho -Password $GH_TOKEN
+dotnet nuget add source "https://nuget.pkg.github.com/hermanho/index.json" --name "GitHub" --username hermanho --password $GH_TOKEN
 dotnet nuget push src/Postal.AspNetCore/bin/Release/*.nupkg --source "GitHub"
 dotnet nuget push src/Postal.AspNetCore/bin/Release/*.nupkg --source https://www.myget.org/F/herman-github/api/v2/package --api-key $NUGET_API_KEY
