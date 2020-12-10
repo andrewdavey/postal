1fcf99657fc4db4e8b46a7924304b30ee7520a77
Herman Ho <hermanho@users.noreply.github.com>
2020-12-11 00:40:47 +0800

    fix FromAddress

--- src/Postal.AspNetCore/EmailService.cs
+++ src/Postal.AspNetCore/EmailService.cs
@@ -99,6 +99,10 @@ namespace Postal
             var rawEmailString = await emailViewRenderer.RenderAsync(email);
             emailParser = new EmailParser(emailViewRenderer);
             var mailMessage = await emailParser.ParseAsync(rawEmailString, email);
+            if (mailMessage.From == null || mailMessage.From.Address == null)
+            {
+                mailMessage.From = new MailAddress(this.options.FromAddress);
+            }
             return mailMessage;
         }
     }
