3ff887247030836292e2f01c0011e34e51b0cbcc
Herman Ho <hermanho@users.noreply.github.com>
2023-03-29 00:13:30 +0100

    fix: migrate to github action

--- scripts/travis-deploy.sh
+++
@@ -1,7 +0,0 @@
-# https://github.com/NuGet/Home/issues/8580#issuecomment-555696372
-for f in  src/Postal.AspNetCore/bin/Release/*.nupkg
-do
-  curl -vX PUT -u "hermanho:$GH_TOKEN" -F package=@$f https://nuget.pkg.github.com/hermanho/
-done
-
-dotnet nuget push src/Postal.AspNetCore/bin/Release/*.nupkg --source https://www.myget.org/F/herman-github/api/v2/package --api-key $NUGET_API_KEY
